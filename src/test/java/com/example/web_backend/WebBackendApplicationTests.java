a26ad65d86ddac4d77c147ca688e1f8f48cf5346
AlwaysKDY <2404954410@qq.com>
2023-06-23 00:54:16 +0800

    111

--- src/test/java/com/example/web_backend/WebBackendApplicationTests.java
+++ src/test/java/com/example/web_backend/WebBackendApplicationTests.java
@@ -1,7 +1,13 @@
 package com.example.web_backend;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class WebBackendApplicationTests {
